83eddb79e719447666272f6fef23549603a41c3f
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2022-06-01 19:09:48 +0300

    Update 01.06.2022 1909

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <?php require './required-files/head.php'; ?>
+        <title>YouTube - clone</title>
     </head>
     <body>
         <?php require './required-files/header.php'; ?>
