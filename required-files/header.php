f3b2719881bc5c44621e0b5f6c0c74ca00a6ab2e
Super-Michael-05 <mikaelkolehmainen05@gmail.com>
2022-06-16 16:06:34 +0300

    Update 16.06.2022

--- required-files/header.php
+++ required-files/header.php
@@ -1,5 +1,13 @@
 <?php
     echo "
+        <header>
+            <!-- HAMBURGER MENU -->
+            <nav>
 
+            </nav>
+            <div class='logo'>
+                <img src='./media/logo.jpg' alt='YouTube logo'>
+            </div>
+        </header>
     ";
 ?>
