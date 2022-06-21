579df6fbad9b5e8021298d8ff5f65abf23a248de
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2022-06-21 18:10:53 +0300

    navigation menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,13 +2,13 @@ function openHamburgerMenu() {
     links = document.getElementById('navLinks');
     hamburgerImg = document.getElementById("hamburgerImage");
     hamburger = document.getElementById("hamburger");
-    if (links.style.display == 'block') {
+    if (links.style.display == 'block' || links.style.display == "") {
         links.style.display = 'none';
         hamburger.classList.add("hamburger-click");
         setTimeout(() => { 
             hamburger.classList.remove("hamburger-click");
         }, 1000);
-    } else if (links.style.display == 'none' || links.style.display == "") {
+    } else if (links.style.display == 'none') {
         links.style.display = 'block';
         hamburger.classList.add("hamburger-click");
         setTimeout(() => { 
--- required-files/header.php
+++ required-files/header.php
@@ -9,7 +9,84 @@
                     </a>
                 </div>
                 <div id='navLinks'>
-                    
+                    <div class='item'>
+                        <i class='fa-solid fa-house'></i>
+                        <a href=''>Home</a>
+                    </div>
+                    <div class='item'>
+                        <i class='fa-solid fa-compass'></i>
+                        <a href=''>Explore</a>
+                    </div>
+                    <div class='item'>
+                        <i class='fa-solid fa-stopwatch'></i>
+                        <a href=''>Shorts</a>
+                    </div>
+                    <div class='item'>
+                        <i class='fa-solid fa-clipboard-list'></i>
+                        <a href=''>Subscriptions</a>
+                    </div>
+                    <hr>
+                    <div class='item'>
+                        <i class='fa-solid fa-book'></i>
+                        <a href=''>Library</a>
+                    </div>
+                    <div class='item'>
+                        <i class='fa-solid fa-clock-rotate-left'></i>
+                        <a href=''>History</a>
+                    </div>
+                    <hr>
+                    <p>Sign in to like videos, comment and subscribe.</p>
+                    <a href='' class='Sign-btn'>
+                        <i class='fa-regular fa-circle-user'></i>
+                        <p>SIGN IN</p>
+                    </a>
+                    <hr>
+                    <p>BEST OF YOUTUBE</p>
+                    <div class='categories'>
+                        <div class='item'>
+                            <i class='fa-solid fa-music'></i>
+                            <a href=''>Music</a>
+                        </div>
+                        <div class='item'>
+                            <i class='fa-solid fa-trophy'></i>
+                            <a href=''>Sports</a>
+                        </div>
+                        <div class='item'>
+                            <i class='fa-solid fa-gamepad'></i>
+                            <a href=''>Gaming</a>
+                        </div>
+                        <div class='item'>
+                            <i class='fa-solid fa-film'></i>
+                            <a href=''>Movies</a>
+                        </div>
+                        <div class='item'>
+                            <i class='fa-solid fa-newspaper'></i>
+                            <a href=''>News</a>
+                        </div>
+                        <div class='item'>
+                            <i class='fa-solid fa-tower-broadcast'></i>
+                            <a href=''>Live</a>
+                        </div>
+                        <div class='item'>
+                            <i class='fa-solid fa-vr-cardboard'></i>
+                            <a href=''>360° Video</a>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class='item'>
+                        <i class='fa-solid fa-plus'></i>
+                        <a href=''>Browse channels</a>
+                    </div>
+                    <hr>
+                    <p>MORE FROM YOUTUBE</p>
+                    <div class='item'>
+                        <i class='fa-brands fa-youtube'></i>
+                        <a href=''>YouTube Premium</a>
+                    </div>
+                    <div class='item'>
+                        <i class='fa-solid fa-tower-broadcast'></i>
+                        <a href=''>Live</a>
+                    </div>
                 </div>
             </nav>
             <div class='logo'>
