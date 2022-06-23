9cfaf02baec443000ca36b50ca36ba6aba9795fc
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2022-06-23 17:08:32 +0300

    started to make the categories

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
     <body>
         <?php require './required-files/header.php'; ?>
         
+        <section>
+            <article class='video-categories'>
+                <div class='category'>
+                    <p>All</p>
+                </div>
+                <div class='category'>
+                    <p>Live</p>
+                </div>
+                <div class='category'>
+                    <p>Music</p>
+                </div>
+            </article>
+        </section>
+
         <?php require './required-files/footer.php'; ?>
     </body>
 </html>
